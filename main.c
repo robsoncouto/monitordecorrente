ee0ec242e9821e5d2c9358011e1002fb810cd307
Robson <robsoncouto@outlook.com>
2018-01-02 17:07:30 -0300

    Serial port working

--- main.c
+++ main.c
@@ -4,15 +4,23 @@
 #include<avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+//#include "monitor.h"
+#include "serial/uart.h"
 
-void init_tim1(void);
+/*
+criterio1 e 2
+tempo 
+*/
 
 int main(void) {
   DDRB=1<<PINB5;
-
+  DDRD=(1<<PINB0)|(1<<PINB1);
+  sei();
+  uart_init(UART_BAUD_SELECT(9600,16000000UL));
   while(1) {
     PORTB^=(1<<PINB5);
     _delay_ms(1000);
+    uart_puts(" hello world\n");
   }
 
 }
